af488c11c05d8cd78a89612e44264a2eca36f647
Dustin Williams <dustin.williams-4@selu.edu>
2014-11-25 03:04:59 -0600

    removed extra flles
    removed extra flles

---
+++ IDScanner/IDScanner/ViewModels/AdminViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IDScanner.ViewModels
+{
+    public class AdminViewModel
+    {
+        public IEnumerable<IDScanner.Faculty> Faculty { get; set; }
+        public IEnumerable<IDScanner.Student> Student { get; set; }
+        public IEnumerable<IDScanner.Attendance> Attendance { get; set; }
+        public IEnumerable<IDScanner.Schedule> Schedule { get; set; }
+        public IEnumerable<IDScanner.Cours> Course { get; set; }
+    }
+}
